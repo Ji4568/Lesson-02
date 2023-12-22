ad6de6cbab941ad168a16d5a35a886b3adb3f8e7
呂佶 <ji8058882@gmail.com>
2023-12-22 15:47:30 +0800

    第三節：簡單資料清理及寫出檔案(2)

--- 3.R
+++ 3.R
@@ -12,3 +12,11 @@ dat[,c("school", "DUP1", "DUP2")]
 #一個一個檢查DUP1與DUP2是否相同有點白痴，這時候我們可以用函數「all.equal」
 all.equal(dat$DUP1, dat$DUP2)
 
+#簡單資料清理及寫出檔案(2)
+#現在，我們會希望把重複的列刪除掉，這時候我們可以使用索引函數做到。
+#由於我們已經創造了一個新變數「DUP」，若他為FALSE就是唯一的個案，TRUE的就是重複的
+
+dat.clean = dat[dat$DUP1==FALSE,]
+#現在，我們已經可以透過函數「write.csv」將清理過的檔案寫出了。
+write.csv(dat.clean, "data2_1 clean.csv", fileEncoding = 'CP950')
+
